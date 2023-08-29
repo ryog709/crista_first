682f4cfd229b7a19f9d44e37d3d229ec51995786
gon <ryog709@gmail.com>
2023-08-30 04:43:44 +0900

    8/29　一通り完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
-
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+// ハンバーガーメニュー
+$(function () {
+  $(".js-hamburger,.js-drawer,.js-drawer a").click(function () {
+    if ($(".js-drawer").hasClass("is-current")) {
+      $(".js-drawer").fadeOut(function () {
+        $(this).removeClass("is-current"); // これで display: none; になる
+      });
+      $(".js-hamburger").removeClass("is-current");
+    } else {
+      $(".js-drawer").hide().addClass("is-current").fadeIn(); // これで display: block; になる
+      $(".js-hamburger").addClass("is-current");
+    }
+  });
+});
 
 });
