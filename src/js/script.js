8fa1b5ab9417ff391ca4a07167afe848cb42b945
gon <ryog709@gmail.com>
2023-09-08 00:45:59 +0900

    9/8完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,28 @@
 jQuery(function ($) {
   // この中であればWordpressでも「$」が使用可能になる
-// ハンバーガーメニュー
-$(function () {
-  $(".js-hamburger,.js-drawer,.js-drawer a").click(function () {
-    if ($(".js-drawer").hasClass("is-current")) {
-      $(".js-drawer").fadeOut(function () {
-        $(this).removeClass("is-current"); // これで display: none; になる
-      });
-      $(".js-hamburger").removeClass("is-current");
-    } else {
-      $(".js-drawer").hide().addClass("is-current").fadeIn(); // これで display: block; になる
-      $(".js-hamburger").addClass("is-current");
+  $(window).resize(function() {
+    if ($(window).width() >= 768) {
+      if ($(".js-drawer").hasClass("is-current")) {
+        $(".js-drawer").fadeOut(500, function() {
+          $(this).removeClass("is-current");
+        });
+        $(".js-hamburger").removeClass("is-current");
+      }
     }
   });
-});
+  // ハンバーガーメニュー
+  $(function () {
+    $(".js-hamburger,.js-drawer,.js-drawer a").click(function () {
+      if ($(".js-drawer").hasClass("is-current")) {
+        $(".js-drawer").fadeOut(500, function () {
+          $(this).removeClass("is-current");
+        });
+        $(".js-hamburger").removeClass("is-current");
+      } else {
+        $(".js-drawer").hide().addClass("is-current").fadeIn(500);
+        $(".js-hamburger").addClass("is-current");
+      }
+    });
+  });
 
 });
